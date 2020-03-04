1cdf2f9434c15a01fb76801247488ae1c43a33ef
Jökull Sólberg Auðunsson <jokull@solberg.is>
2020-03-04 23:08:30 +0000

    Adding production frontend bundles to git

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./templates/*.html"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };

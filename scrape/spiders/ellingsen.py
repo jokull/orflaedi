b044fa21099a5cadcd377ae137c37910ab20a8f8
Jökull Sólberg Auðunsson <jokull@solberg.is>
2020-07-01 08:33:30 +0000

    Improve Ellingsen scraper

--- scrape/spiders/ellingsen.py
+++ scrape/spiders/ellingsen.py
@@ -6,18 +6,14 @@ class EllingsenSpider(scrapy.Spider):
     name = "ellingsen"
 
     start_urls = [
-        "https://ellingsen.s4s.is/ellingsen/rafhjol/mate",
-        "https://ellingsen.s4s.is/ellingsen/rafhjol/zero",
-        "https://ellingsen.s4s.is/ellingsen/rafhjol/merida",
-        "https://ellingsen.s4s.is/ellingsen/rafhjol/tern",
-        "https://ellingsen.s4s.is/ellingsen/rafhjol/e-fati",
-        "https://ellingsen.s4s.is/ellingsen/rafhjol/legend",
-        "https://ellingsen.s4s.is/ellingsen/rafhjol/gpad",
+        "https://ellingsen.s4s.is/ellingsen/rafhjol/oell-rafhjol",
     ]
 
     def parse(self, response):
-        for link in response.css(".products-container .product>a::attr(href)"):
-            yield response.follow(link, self.parse_product)
+        for el in response.css(".products-container .product"):
+            if el.css(".product_Discount_Text::text").get() == "Uppselt":
+                continue
+            yield response.follow(el.css("a::attr(href)")[1], self.parse_product)
 
     def parse_product(self, response):
 
@@ -31,9 +27,34 @@ class EllingsenSpider(scrapy.Spider):
             url = w3lib_url.add_or_replace_parameters(url, {"Width": "2400", "Height": "2400"})
             file_urls.append(url)
 
+        name = response.css(".information>.product-name::text").get()
+        make = None
+
+        for trim in ("rafhlaupahjól", "rafhjól"):
+            if name.endswith(f" {trim}"):
+                name = name[:-len(f" {trim}")]
+
+        if name == "Mate X Bike":
+            make, name = "Mate", "Mate X"
+
+        if name == "Legend eBike Milano":
+            make, name = "Legend", "Milano"
+
+        if name == "Legend eBike Monza":
+            make, name = "Legend", "Monza"
+
+        if name == "Legend eBike Siena":
+            make, name = "Legend", "Siena"
+
+
+        for _make in ("Tern", "Zero"):
+            if name.startswith(_make):
+                make, name = _make, name
+
         yield {
             "sku": response.css(".information>.tiny-productID::text").get(),
-            "name": response.css(".information>.product-name::text").get(),
+            "name": name,
+            "make": make,
             "price": price,
             "file_urls": file_urls,
             "scrape_url": response.url,

bc1197bb7c5d52cac3464f1124a5ea5a2a4e377c
Jökull Sólberg <jokull@solberg.is>
2021-03-23 15:13:11 +0000

    Banner for Tri

--- static/dist/styles.js
+++ static/dist/styles.js
@@ -222,7 +222,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62481" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64631" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
